880b3117aa4c570450c50bb5379a32f447454d4d
Sam McCullough <samanuel.mc@gmail.com>
2019-11-25 16:27:57 -0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Sam on 2019-11-25.
+//  Copyright © 2019 Sailfish Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
